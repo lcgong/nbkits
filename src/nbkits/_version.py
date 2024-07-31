b2c68e102359c46b980e99f5d8313c0183633f68
Lyu Chenggong <lcgong@tiangong.edu.cn>
2024-07-31 23:22:01 +0800

    临时提交

--- src/nbkits/_version.py
+++ src/nbkits/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev1+g3f0452a.d20240731'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g3f0452a.d20240731')
+__version__ = version = '0.2.1.dev0'
+__version_tuple__ = version_tuple = (0, 2, 1, 'dev0')
